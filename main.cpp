e51394154559f2d480c90e84034bad0168bcc325
Nata <proskurinanata1997@mail.ru>
2020-06-07 14:26:35 +0700

    Реализован тестовый пример

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <QString>
+#include <QTextStream>
+#include <byFileTypeStrategy.h>
+#include <byFolderStrategy.h>
+#include <test.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    Test test;
+    return 0;
+}
